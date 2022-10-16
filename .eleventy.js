ee971ac637eb8e12814f9af7e8209564bdcaba02
Aleksios22 <alexisrochon98@outlook.com>
2022-10-16 13:15:22 -0400

    heyy

---
+++ .eleventy.js
@@ -0,0 +1,13 @@
+module.exports = function(eleventyConfig) {
+    
+    eleventyConfig.addPassthroughCopy('./src/assets');
+    eleventyConfig.addPassthroughCopy('./src/admin');
+    
+    
+    return {
+        dir: {
+            input: "src",
+            output: "public"
+        }
+    };
+}
